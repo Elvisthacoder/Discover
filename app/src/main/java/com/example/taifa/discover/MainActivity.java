34c136f77bd7deb0c335bdc7d4a60bc982875922
Elvisthacoder <ekk213@gmail.com>
2016-04-18 10:23:32 +0300

    array_list

--- app/src/main/java/com/example/taifa/discover/MainActivity.java
+++ app/src/main/java/com/example/taifa/discover/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.taifa.discover;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initUI();
     }
-}
+
+    private void initUI() {
+
+        final SwitchCompat switchColored = (SwitchCompat) findViewById(R.id.home_switch_colored);
+        final SwitchCompat switchFourItems = (SwitchCompat) findViewById(R.id.home_switch_four_items);
+        final AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
+        final TextView currentItem = (TextView) findViewById(R.id.home_current_item);
+        final ArrayList<AHBottomNavigationItem> items = new ArrayList<>();
+
+    }
