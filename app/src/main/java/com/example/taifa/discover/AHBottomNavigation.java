60198c5e08809b0df5938ca479cbb1cd310e601b
Elvisthacoder <ekk213@gmail.com>
2016-04-18 11:19:04 +0300

    static_variables

--- app/src/main/java/com/example/taifa/discover/AHBottomNavigation.java
+++ app/src/main/java/com/example/taifa/discover/AHBottomNavigation.java
@@ -1,7 +1,16 @@
 package com.example.taifa.discover;
 
+import android.widget.FrameLayout;
+
 /**
  * Created by taifa on 4/18/16.
  */
-public class AHBottomNavigation {
+public class AHBottomNavigation extends FrameLayout{
+
+    // Static
+    private static String TAG = "AHBottomNavigation";
+    private static final int MIN_ITEMS = 3;
+    private static final int MAX_ITEMS = 5;
+
+
 }
