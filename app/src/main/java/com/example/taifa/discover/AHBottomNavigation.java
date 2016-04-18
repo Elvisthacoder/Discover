8b674f36356cf7e834d6eba27bc8ae42476ba466
Elvisthacoder <ekk213@gmail.com>
2016-04-18 11:20:28 +0300

    private_variables

--- app/src/main/java/com/example/taifa/discover/AHBottomNavigation.java
+++ app/src/main/java/com/example/taifa/discover/AHBottomNavigation.java
@@ -1,6 +1,10 @@
 package com.example.taifa.discover;
 
+import android.content.Context;
+import android.view.View;
 import android.widget.FrameLayout;
+
+import java.util.ArrayList;
 
 /**
  * Created by taifa on 4/18/16.
@@ -15,6 +19,13 @@ public class AHBottomNavigation extends FrameLayout{
     // Listener
     private AHBottomNavigationListener listener;
     private OnTabSelectedListener tabSelectedListener;
+
+    // Variables
+    private Context context;
+    private ArrayList<AHBottomNavigationItem> items = new ArrayList<>();
+    private ArrayList<View> views = new ArrayList<>();
+    private View backgroundColorView;
+    private boolean colored = false;
 
 
 }
