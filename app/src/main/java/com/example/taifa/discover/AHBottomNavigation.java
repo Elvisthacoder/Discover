945e59b18bc3b25f050e57872a98b75c9d919b4f
Elvisthacoder <ekk213@gmail.com>
2016-04-18 14:55:41 +0300

    icon_drawable

--- app/src/main/java/com/example/taifa/discover/AHBottomNavigation.java
+++ app/src/main/java/com/example/taifa/discover/AHBottomNavigation.java
@@ -275,6 +275,10 @@ public class AHBottomNavigation extends FrameLayout{
                     currentColor = item.getColor();
                 }
 
+                icon.setImageDrawable(AHHelper.getTintDrawable(context, items.get(i).getResource(),
+                        currentItem == i ? ContextCompat.getColor(context, R.color.colorActiveSmall) :
+                                ContextCompat.getColor(context, R.color.colorInactiveSmall)));
+
 
 
         }
