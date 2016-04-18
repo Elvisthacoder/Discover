24d2cbddd200c0c06c99ad2d2511ce2844b3b47b
Elvisthacoder <ekk213@gmail.com>
2016-04-18 10:25:13 +0300

    graphics_color

--- app/src/main/java/com/example/taifa/discover/MainActivity.java
+++ app/src/main/java/com/example/taifa/discover/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.taifa.discover;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
@@ -27,5 +28,12 @@ public class MainActivity extends AppCompatActivity {
         final AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
         final TextView currentItem = (TextView) findViewById(R.id.home_current_item);
         final ArrayList<AHBottomNavigationItem> items = new ArrayList<>();
+
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem("Label One", R.drawable.ic_maps_place, Color.parseColor("#455C65"));
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Label Two", R.drawable.ic_maps_local_bar, Color.parseColor("#00886A"));
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("Label Three", R.drawable.ic_maps_local_restaurant, Color.parseColor("#8B6B62"));
+        final AHBottomNavigationItem item4 = new AHBottomNavigationItem("Label Four", R.drawable.ic_maps_local_bar, Color.parseColor("#6C4A42"));
+        final AHBottomNavigationItem item5 = new AHBottomNavigationItem("Label Five", R.drawable.ic_maps_place, Color.parseColor("#F63D2B"));
+
 
     }
